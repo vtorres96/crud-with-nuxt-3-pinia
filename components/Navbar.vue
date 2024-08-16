9caf1d6cf21de521017a1f18ad89fc5ff30caf03
Victor Torres <victor.torres_taking@totalexpress.com.br>
2024-08-16 15:05:35 -0300

    added Navbar component

---
+++ components/Navbar.vue
@@ -0,0 +1,20 @@
+<template>
+  <VAppBar>
+    <span @click="navigateTo('/')">{{ title }}</span>
+  </VAppBar>
+</template>
+
+<script setup lang="ts">
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  title: {
+    type: String,
+    required: true
+  }
+});
+</script>
+
+<style scoped>
+
+</style>
