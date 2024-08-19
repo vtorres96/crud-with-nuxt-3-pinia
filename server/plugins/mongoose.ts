a3a25cefccdcabbde2b69bfcdf98a6ad78d3b533
Victor Torres <victor.torres_taking@totalexpress.com.br>
2024-08-19 13:10:04 -0300

    adicionando try/catch para analisar motivo de erro caso houver falha na conexao com banco de dados

--- server/plugins/mongoose.ts
+++ server/plugins/mongoose.ts
@@ -1,8 +1,12 @@
 import {connect} from "mongoose";
 
 export default defineNitroPlugin(async(nitroApp)=>{
-  const runtimeConfig = useRuntimeConfig()
-  
-  await connect(runtimeConfig.MONGO_URI)
-  console.log('MONGO DB conectado!');
+  try {
+      const runtimeConfig = useRuntimeConfig()
+      
+      await connect(runtimeConfig.MONGO_URI)
+      console.log('MONGO DB conectado!');
+  } catch (error: any) {
+    console.log(`Falha ao conectar-se ao banco, detalhes: ${error.message}`);
+  }
 })
